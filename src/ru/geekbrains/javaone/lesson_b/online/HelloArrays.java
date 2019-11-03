77e3c76a9a43af664985caa870689c4d46d6d83d
Nikita <guschinnick@gmail.com>
2019-11-03 18:38:38 +0300

    Задание02

--- src/ru/geekbrains/javaone/lesson_b/online/HelloArrays.java
+++ src/ru/geekbrains/javaone/lesson_b/online/HelloArrays.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class HelloArrays {
 //-----------1.Превращаем 0 в 1 и наоборот
-    public static int[] invertArray(int[] arr){
+    public static void invertArray(int[] arr){
         for(int i=0;i<arr.length;i++){
             if(arr[i]==0){
                 arr[i]=1;
@@ -14,14 +14,22 @@ public class HelloArrays {
                 arr[i]=0;
             }
         }
-        return arr;
+    }
+    public static void fillArray( int[] arr){
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = 1 + i *3;
+        }
     }
 
     public static void main(String[] args){
         int[] arr={1,1,0,0,1,0,1,1,0,0};
-        System.out.println("--Задание 1---");
-        System.out.println("Создали массив:  " + Arrays.toString(arr));
-        System.out.println("Поменяли массив: "+Arrays.toString(invertArray(arr)));
+        System.out.println("--Задание 1---\nСоздали массив:  " + Arrays.toString(arr));
+        invertArray(arr);
+        System.out.println("Поменяли массив: "+Arrays.toString(arr)+"\n");
+        int[] arr2=new int[8];
+        fillArray(arr2);
+        System.out.println("--Задание 2---\nАвтозаполнение пустого массива:  " + Arrays.toString(arr)+"\n");
+         
     }
 
 }
