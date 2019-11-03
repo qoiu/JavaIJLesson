6d3fbb27ba08ada924ee82874691fc2c65ae10a0
Nikita <guschinnick@gmail.com>
2019-11-03 19:23:06 +0300

    Задание05

--- src/ru/geekbrains/javaone/lesson_b/online/HelloArrays.java
+++ src/ru/geekbrains/javaone/lesson_b/online/HelloArrays.java
@@ -20,6 +20,32 @@ public class HelloArrays {
             arr[i] = 1 + i *3;
         }
     }
+    public static void multiplyArray(int[] arr){
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = (arr[i]<6)? arr[i]*2:arr[i];
+        }
+    }
+    public static int returnMin(int[] arr){
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++){
+                min=(min>arr[i])?arr[i]:min;
+        }
+        return min;
+    }
+    public static int returnMax(int[] arr){
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            max=(max<arr[i])?arr[i]:max;
+        }
+        return max;
+    }
+    public static void diagonal(int[][] arr){
+        for(int i=0;i<arr.length;i++){
+            arr[i][i]=1;
+            arr[i][arr.length-i-1]=1;
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
 
     public static void main(String[] args){
         int[] arr={1,1,0,0,1,0,1,1,0,0};
@@ -29,7 +55,14 @@ public class HelloArrays {
         int[] arr2=new int[8];
         fillArray(arr2);
         System.out.println("--Задание 2---\nАвтозаполнение пустого массива:  " + Arrays.toString(arr)+"\n");
-         
+        int[] arr3={ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("--Задание 3---\nВ массиве      :  " + Arrays.toString(arr3));
+        multiplyArray(arr3);
+        System.out.println("Умножаем всех <6 на 2:  " + Arrays.toString(arr3)+"\n");
+        int[] arr4={ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("--Задание 4---\nМинимальное значение массива:  " + returnMin(arr4)+"\nМаксимальное значение массива:"+ returnMax(arr4));
+        int[][] arr5=new int[11][11];
+        diagonal(arr5);
     }
 
 }
