22dece328eb5a776172236c86e35d3c1a1e05dd1
Nikita <guschinnick@gmail.com>
2019-11-06 16:53:37 +0300

    AI

--- src/ru/geekbrains/javaone/lesson__c/online/TicTacToeMy.java
+++ src/ru/geekbrains/javaone/lesson__c/online/TicTacToeMy.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.javaone.lesson__c.online;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -18,7 +19,7 @@ public class TicTacToeMy {
     private static void initField(){
         fieldSizeX=6;
         fieldSizeY=6;
-        lengthWin=3;
+        lengthWin=4;//>=3
         fieldValue=new int[fieldSizeY][fieldSizeX];
         field = new char[fieldSizeY][fieldSizeX];
         for (int i = 0; i < fieldSizeY; i++) {
@@ -34,6 +35,7 @@ public class TicTacToeMy {
                 System.out.print(field[i][j] + "|");
             }
         }
+        System.out.print("\n-----------");
     }
     private static void showValue(){
         for(int i=0;i<fieldSizeY;i++){
@@ -42,6 +44,7 @@ public class TicTacToeMy {
                 System.out.print(fieldValue[i][j] + "|");
             }
         }
+        System.out.print("\n----------");
     }
     private static void humanTurn(){
         int x,y;
@@ -57,61 +60,104 @@ public class TicTacToeMy {
         return c == DOT_EMPTY;
     }
     private static boolean isinField(int x, int y) {
-        return x > 0 && x < fieldSizeX && y > 0 && x < fieldSizeY;
+        return x >= 0 && x < fieldSizeX && y >= 0 && x < fieldSizeY;
     }
    //проверяем пустоту следующих lengthWin элементов
-    private static boolean checkNext(int x, int y, int plusX, int plusY) {
+
+   private static int countEnemy(int x,int y,int plusX,int plusY) {
+       int i = 1;
+       for (int k = 0; k < lengthWin; k++) {
+           if (field[y + k * plusY][x + k * plusX] == DOT_HUMAN) {
+               i *= 3;
+           } else if (field[y + k * plusY][x + k * plusX] == DOT_AI) {
+               return 0;
+           }
+       }
+       return i;
+   }
+    private static int canWin(int x,int y,int plusX,int plusY){
+        int i = 0;
         for (int k = 0; k < lengthWin; k++)
-            if (field[x + k*plusY][y + k*plusX] != DOT_EMPTY) {
-                return false;
-            }
-        return true;
+            if (field[y + k * plusY][x + k * plusX] == DOT_AI)
+                i += 1;
+            if(lengthWin-i==1)
+                return 50;
+            return 0;
+
     }
 
+    private static void fillNext(int x,int y,int plusX,int plusY){
+        for (int k = 0; k < lengthWin; k++) {
+            if (isEmpty(field[y + k * plusY][x + k * plusX]))
+                fieldValue[y + k * plusY][x + k * plusX] += countEnemy(x, y, plusX, plusY);
+            if (isEmpty(field[y + k * plusY][x + k * plusX]))
+                fieldValue[y + k * plusY][x + k * plusX] += canWin(x,y,plusX,plusY);
+        }
+    }
     private static void aiTurn(){
+        fieldCheckValue();
+        int pX=0;
+        int pY=0;
+        int max=0;
+        for (int y=0;y<fieldSizeY;y++){
+            for (int x=0;x<fieldSizeX;x++) {
+                if(fieldValue[y][x]>max){
+                    pX=x;
+                    pY=y;
+                    max=fieldValue[y][x];
+    }
+}
+        }
 
+                field[pY][pX]=DOT_AI;
+                showField();
+    }
+    private static void clearFieldValue(){
+        for (int y=0;y<fieldSizeY;y++){
+            for (int x=0;x<fieldSizeX;x++) {
+            fieldValue[y][x]=0;
+            }
+        }
     }
     private static void fieldCheckValue(){
+        clearFieldValue();
         checkDiagonal();
         checkHor();
-       checkVert();
+        checkVert();
     }
-
     private static void checkDiagonal(){
         for (int i=0;i<=fieldSizeY-lengthWin;i++){
             for (int j=0;j<=fieldSizeX-lengthWin;j++){
-                if (checkNext(i,j,1,1))
-                    for (int k = 0; k < lengthWin; k++) fieldValue[i+k][j+k] += 1;
-                    //обратная диагональ
-               if (checkNext(fieldSizeX-1-j,j,1,-1))
-                    for (int k = 0; k < lengthWin; k++) fieldValue[i+k][fieldSizeX-1-(j +k)] += 1;
+                fillNext(j,i,1,1);
+                //обратная диагональ
+                fillNext(fieldSizeX-1-j,i,-1,1);
             }
         }
     }
     private static void checkVert() {
-        for (int i = 0; i < fieldSizeX; i++) {
-            for (int j = 0; j <= fieldSizeY-lengthWin; j++) {
-                if (checkNext(i,j,1,0))
-                    for (int k = j; k < lengthWin+j; k++) fieldValue[k][i] += 1;
+        for (int i = 0; i <= fieldSizeY-lengthWin; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                    fillNext(j,i,0,1);
+
             }
         }
     }
     private static void checkHor() {
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j <= fieldSizeX-lengthWin; j++) {
-                if (checkNext(j,i,0,1))
-                    for (int k = j; k < lengthWin+j; k++) fieldValue[i][k] += 1;
+                    fillNext(j,i,1,0);
             }
         }
     }
 
     public static void main(String[] args){
         initField();
-
-       //humanTurn();
-        showField();
-        fieldCheckValue();
-        showValue();
+        while (true) {
+            humanTurn();
+            //
+            aiTurn();
+            showValue();
+        }
     }
 
 }
