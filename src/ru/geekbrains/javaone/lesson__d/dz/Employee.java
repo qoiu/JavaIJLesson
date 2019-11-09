b62e0a66345299da5f1ed6da4cfc23d55b245887
Nikita <guschinnick@gmail.com>
2019-11-09 09:54:50 +0300

    itog

--- src/ru/geekbrains/javaone/lesson__d/dz/Employee.java
+++ src/ru/geekbrains/javaone/lesson__d/dz/Employee.java
@@ -6,13 +6,20 @@ public class Employee {
     private String phone;
     private int salary;
     private int birthYear;
+    private int empId;
+    private static int id;
 
     Employee(String name,String position,String phone,int salary, int age){
+        empId=id;
         this.name=name;
         this.position=position;
         this.phone=phone;
         this.salary=salary;
         this.birthYear=2019-age;
+        id+=1;
+    }
+    public int getId(){
+        return empId;
     }
     public String getName(){
         return name;
@@ -29,6 +36,8 @@ public class Employee {
     public int getAge(){
         return 2019-birthYear;
     }
-
+    public void setSalary(int salary){
+        this.salary=salary;
+    }
 
 }
