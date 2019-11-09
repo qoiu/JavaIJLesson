2658dfdb3ff1c745f525328c04e8fcef95945080
Nikita <guschinnick@gmail.com>
2019-11-09 09:04:42 +0300

    zad 2-3

--- src/ru/geekbrains/javaone/lesson__d/dz/DZ04.java
+++ src/ru/geekbrains/javaone/lesson__d/dz/DZ04.java
@@ -2,6 +2,8 @@ package ru.geekbrains.javaone.lesson__d.dz;
 
 public class DZ04 {
     public static void main(String[] args){
+        Employee emp1=new Employee("Клещёнок Ирина владимировна","бухгалтер","+79219799092",25000,32);
+        emp1.getName();
 
     }
 }
--- src/ru/geekbrains/javaone/lesson__d/dz/Employee.java
+++ src/ru/geekbrains/javaone/lesson__d/dz/Employee.java
@@ -1,18 +1,34 @@
 package ru.geekbrains.javaone.lesson__d.dz;
 
 public class Employee {
-    String last_name;
-    String first_name;
-    String second_name;
-    String position;
-    int phone;
-    int salary;
-    int age;
+    private String name;
+    private String position;
+    private String phone;
+    private int salary;
+    private int birthYear;
 
-    Employee(String name,String position,int phone,int salary, int age){
+    Employee(String name,String position,String phone,int salary, int age){
+        this.name=name;
         this.position=position;
         this.phone=phone;
         this.salary=salary;
-        this.age=age;
+        this.birthYear=2019-age;
     }
+    public String getName(){
+        return name;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public int getAge(){
+        return 2019-birthYear;
+    }
+
+
 }
