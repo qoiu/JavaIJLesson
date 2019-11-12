ba6994123f64c3dcbd9223ea47456e1dd2ce420c
Nikita <guschinnick@gmail.com>
2019-11-12 06:17:43 +0300

    itog

--- src/ru/geekbrains/javaone/lesson_c/home/Animal.java
+++ src/ru/geekbrains/javaone/lesson_c/home/Animal.java
@@ -1,10 +1,10 @@
 package ru.geekbrains.javaone.lesson_c.home;
 
 public abstract class Animal {
-    protected String name;
-    protected int speedLimit;
-    protected float jumpLimit;
-    protected int swimLimit;
+    private String name;
+    private int speedLimit;
+    private float jumpLimit;
+    private int swimLimit;
 
 
    protected Animal(String name,int speedLimit, float jumpLimit,int swimLimit){
@@ -19,11 +19,20 @@ public abstract class Animal {
     protected Animal(){}
 
 
-    public void run(){
-        System.out.println(name+" run");
-    }
-    public void swim(){
-        System.out.println(name+" swim");
+    public void run(int distance){
+       if(distance<speedLimit) {
+           System.out.println(name + " run");
+       }else{
+           System.out.println(name + " don't run");
+       }
+       }
+    public void swim(int distance){
+       if(swimLimit==0){
+           System.out.println(name+" can't swim");
+       } else if(distance<swimLimit){
+           System.out.println(name+" swim");
+       }
+
     }
     public void jump(){
         System.out.println(name+" jump");
@@ -31,5 +40,10 @@ public abstract class Animal {
     public void setName(String name){
         this.name=name;
     }
-
+    public void train(int speedLLim,float jumpLimit,int swimLimit){
+       System.out.println(name+" finish train");
+       this.speedLimit+=speedLLim;
+       this.jumpLimit+=jumpLimit;
+       this.swimLimit+=swimLimit;
+    }
 }
--- src/ru/geekbrains/javaone/lesson_c/home/DZ05.java
+++ src/ru/geekbrains/javaone/lesson_c/home/DZ05.java
@@ -6,13 +6,23 @@ import ru.geekbrains.javaone.lesson_c.home.zoo.Dog;
 
 public class DZ05 {
     public static void main(String[] args){
+        Dog champ=new Dog("Scooby");
         Animal[] zoo={
                 new Cat("barsic"),
                 new Dog("Reks"),
-                new Bird("sss",200,0.3f,0)
+                new Bird("Kesha",20,0.3f,0)
         };
         for(int i=0;i<zoo.length;i++){
-            zoo[i].jump();
+           //zoo[i].jump();
+            zoo[i].run(150);
         }
+        zoo[2].train(140,0,0);
+        zoo[1].train(-400,0,0);
+        for(int i=0;i<zoo.length;i++){
+            //zoo[i].jump();
+            zoo[i].run(150);
+
+        }
+        champ.run(230);
     }
 }
