4133f5bf9e59e21033cde168fe5c89861bfb2d49
Nikita <guschinnick@gmail.com>
2019-11-19 06:43:53 +0300

    add GameWindow

---
+++ src/ru/geekbrains/javaone/lesson_f/home/GameWindow.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.javaone.lesson_f.home;
+
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    private static final int GW_heigth=570;
+    private static final int GW_width=550;
+    private static final int GW_x=250;
+    private static final int GW_y=250;
+
+
+    GameWindow{
+        setBounds(GW_x,GW_y,GW_width,GW_heigth);
+        setVisible(true);
+        setResizable(false);
+    }
+    public static void main(String[] args){
+        GameWindow.
+    }
+}
